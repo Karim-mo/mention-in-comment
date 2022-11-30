8a330d725d97170534e50165cd826f1eb80ce855
karim-mo <karim.mohamed420@gmail.com>
2022-11-30 17:24:34 +0200

    feat: mentions auto completion inside a comment

---
+++ src/providers/mentions-auto-completion.provider.ts
@@ -0,0 +1,39 @@
+import * as vscode from 'vscode';
+
+export const autoCompleteMentionsInCommentProvider = vscode.languages.registerCompletionItemProvider(
+	{ scheme: 'file' },
+	{
+		// eslint-disable-next-line no-unused-vars
+		provideCompletionItems(document, position, token, context) {
+			const linePrefix = document.lineAt(position).text.substring(0, position.character);
+			const doubleSlashIndex = linePrefix.lastIndexOf('//');
+
+			if (
+				!linePrefix.endsWith(' @') ||
+				doubleSlashIndex === -1 ||
+				doubleSlashIndex > linePrefix.lastIndexOf(' @')
+			) {
+				return undefined;
+			}
+
+			const generateCompletionItem = (text: string) => {
+				let item = new vscode.CompletionItem(text, vscode.CompletionItemKind.Text);
+				item.range = new vscode.Range(position, position);
+				item.command = {
+					command: 'extension.sendEmailToMentionedUser',
+					title: '',
+					arguments: [text],
+				};
+
+				return item;
+			};
+
+			return [
+				generateCompletionItem('mention 1'),
+				generateCompletionItem('mention 2'),
+				generateCompletionItem('mention 3'),
+			];
+		},
+	},
+	'@'
+);
